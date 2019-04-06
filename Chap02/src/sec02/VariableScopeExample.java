2133390ed931a510cc816534fda14b01bfe4d53f
GeonKimdcu <geon0599@cu.ac.kr>
2019-04-07 00:06:07 +0900

    chap02

---
+++ Chap02/src/sec02/VariableScopeExample.java
@@ -0,0 +1,14 @@
+//변수의 사용 범위
+package sec02;
+
+public class VariableScopeExample {
+
+	public static void main(String[] args) {
+		int v1 = 15;
+		if( v1 > 10) {
+			int v2 = v1 - 10;
+		}
+		//int v3 = v1 + v2 + 5;   	 v2변수는 if메소드 블록에서만 사용가능해서 컴파일 에러 발생!!
+	}
+
+}
