d2d9d02341bea4bc1e4e4be90bf7c5b89439100c
GeonKimdcu <geon0599@cu.ac.kr>
2019-04-13 17:12:58 +0900

    chap06

---
+++ Chap06/src/sec08/CalculatorExample1.java
@@ -0,0 +1,11 @@
+package sec08;
+// Calculator의 execute() 실행
+public class CalculatorExample1 {
+
+	public static void main(String[] args) {
+		Calculator1 myCalc = new Calculator1();
+		myCalc.execute(); // Calculator의 execute() 메소드 호출
+
+	}
+
+}
