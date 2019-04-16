f0a3984316f9a9fb20aede03dbe5039f05b2b443
GeonKimdcu <geon0599@cu.ac.kr>
2019-04-17 01:22:28 +0900

    chap06 확인문제

---
+++ Chap06/src/exam/Ex14.java
@@ -0,0 +1,15 @@
+package exam;
+
+public class Ex14 {
+	// 필드	
+	String name;
+		String id;
+		String password;
+		int age;
+		
+		// 생성자
+		Ex14(String name, String id) {
+			 this.name = name;
+			 this.id = id;
+		}
+}
---
+++ Chap06/src/exam/Printer2.java
@@ -0,0 +1,17 @@
+package exam;
+
+public class Printer2 {
+		static void println(int value) {
+			System.out.println(value);
+		}
+		static void println(boolean value) {
+			System.out.println(value);
+		}
+		static void println(double value) {
+			System.out.println(value);
+		}
+		static void println(String value) {
+			System.out.println(value);
+		}
+		
+}
