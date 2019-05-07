ad0691428c37eeff2e2027ca688de5215ce18355
GeonKimdcu <geon0599@cu.ac.kr>
2019-05-07 23:44:55 +0900

    chap07-3 부모 생성자 호출 Student

---
+++ Chap07/src/ch07_3_People/People.java
@@ -0,0 +1,11 @@
+package ch07_3_People;
+
+public class People {
+	public String name;
+	public String ssn;
+	
+	public People(String name, String ssn) {
+		this.name = name;
+		this.ssn = ssn;
+	}
+}
