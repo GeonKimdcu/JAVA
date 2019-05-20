05e19c819a3aa6fc399d69cf49f5f5fe17337216
GeonKimdcu <geon0599@cu.ac.kr>
2019-05-20 15:09:55 +0900

    chap07.7.1 자동타입변환(Promotion) Parent

---
+++ Chap07/src/ch07_7_1_Parent/Child.java
@@ -0,0 +1,11 @@
+package ch07_7_1_Parent;
+
+public class Child extends Parent{
+	@Override
+	public void method2() {
+		System.out.println("Child-method2()");  // ¿Á¡§¿«
+	}
+	public void method3() {
+		System.out.println("Child-method3()");
+	}
+}
