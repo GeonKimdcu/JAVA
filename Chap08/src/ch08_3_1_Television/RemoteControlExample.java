eaf088c7a5c2390500aefd2b162d5b4c4a229268
GeonKimdcu <geon0599@cu.ac.kr>
2019-05-21 16:19:13 +0900

    ch08.3.1 인터페이스 구현

---
+++ Chap08/src/ch08_3_1_Television/RemoteControlExample.java
@@ -0,0 +1,11 @@
+package ch08_3_1_Television;
+public class RemoteControlExample {
+
+	public static void main(String[] args) {
+		RemoteControl rc;
+		rc = new Television();
+		rc = new Audio();
+
+	}
+
+}
