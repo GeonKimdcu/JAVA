9f397841f9aa76af8720a182d10349760bb69d17
GeonKimdcu <geon0599@cu.ac.kr>
2019-05-28 16:34:18 +0900

    chap9.4 중첩 인터페이스

---
+++ Chap09/src/ch09_4_Button/Button.java
@@ -0,0 +1,17 @@
+package ch09_4_Button;
+
+public class Button {
+	OnClickListener listener;
+	
+	void setOnClickListener(OnClickListener listener) {
+		this.listener = listener;
+	}
+	
+	void touch() {
+		listener.onClick();
+	}
+	
+	interface OnClickListener {
+		void onClick();
+	}
+}
