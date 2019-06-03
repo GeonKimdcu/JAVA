231cce0c30443311b9cfbdc35554a4b1a9e95c74
GeonKimdcu <geon0599@cu.ac.kr>
2019-06-03 16:34:49 +0900

    chap10 예외처리

---
+++ Chap10/src/ch10_2_NullPointerException/NullPointerExceptionExample.java
@@ -0,0 +1,11 @@
+package ch10_2_NullPointerException;
+
+public class NullPointerExceptionExample {
+
+	public static void main(String[] args) {
+		String data = null;
+		System.out.println(data.toString());
+
+	}
+
+}
